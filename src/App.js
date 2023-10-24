963d61b21cb94d4111b9db08bea40070d5d830d2
kag0224 <101907323+kag0224@users.noreply.github.com>
2023-10-23 20:57:25 -0700

    Added search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary App</header>
+      <div className="container">
+        <header className="App-header">Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Kristy-Ann</small>
+        </footer>
+      </div>
     </div>
   );
 }
